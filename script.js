d250e38eb4bc1819c632be4cdd5d9389422de93a
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-15 13:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+const sounds=["applause","boo","gasp","tada","victory","wrong"];
+
+const buttonsDiv=document.querySelector("#buttons");
+
+const audioElements = {};
+
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.innerText = sound;
+  btn.classList.add("btn");
+
+  const audio = new Audio(`sounds/${sound}.mp3`);
+  audioElements[sound] = audio;
+
+  btn.addEventListener("click", () => {
+    stopAllSounds();
+    audio.play();
+  });
+
+  buttonsDiv.appendChild(btn);
+});
+
+	
+const stopBtn = document.createElement("button");
+stopBtn.innerText = "Stop";
+stopBtn.classList.add("stop");
+
+stopBtn.addEventListener("click", stopAllSounds);
+
+buttonsDiv.append(stopBtn);
+
+function stopAllSounds() {
+  for (let sound in audioElements) {
+    audioElements[sound].pause();
+    audioElements[sound].currentTime = 0;
+  }
+}
+
+
+
+
